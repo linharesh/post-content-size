0047993a3c517c6831b2078cd325adf7252b5582
Henrique Linhares <Henrique Linhares>
2017-06-15 19:25:55 -0300

    Script funcionando. Renomeando o projeto para post-content-size

--- index.js
+++ index.js
@@ -2,22 +2,13 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
-
-
+var contentSize = 0;
 
 app.post('/reads', function (req, res) {
-
-var stream = fs.createWriteStream("my_file.txt");
-        stream.once('open', function(fd) {
-        stream.write("My first row\n");
-        stream.write("My second row\n");
-        //stream.write(req);
-        stream.end();
-    });
-    console.log("opa");
+    contentSize = contentSize + Number(req.headers['content-length']);
+    console.log(' $ #  $ #  $ #  $ #  $ #  $ # ');
+    console.log(contentSize+ ' bytes');
     res.send("hello people!");
-
 });
-
 
 app.listen(80, function () {});
