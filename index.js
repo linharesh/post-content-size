a021ab8335555aa8936b5726da699c2f0fb810b3
Henrique do Prado Linhares <linhares.h@gmail.com>
2017-07-05 21:03:24 -0300

    Exibindo apenas o valor do content-length sem o somatório

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
-
-var contentSize = 0;
 
 app.post('/reads', function (req, res) {
-    contentSize = contentSize + Number(req.headers['content-length']);
-    console.log(' $ #  $ #  $ #  $ #  $ #  $ # ');
-    console.log(contentSize+ ' bytes');
-    res.send("hello people!");
+    console.log(req.headers['content-length'] + ' bytes');
+    res.send('{"table": {"objectType": "ReadAck","response": "success"},"modifiable": true}');
 });
 
 app.listen(80, function () {});
